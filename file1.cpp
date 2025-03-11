cb12ea7f1bfcc6f49dcd71d4693504ae705da24f
Razfar14 <rajarangkuty14@gmail.com>
2025-03-11 19:59:47 +0700

    first commit

---
+++ file1.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+float Luas, Panjang, Lebar;
+void InputData()
+{
+    cout << "Masukkan Panjang :";
+    cin >> Panjang;
+    cout << "Masukkan Lebar :";
+    cin >> Lebar;
+}
+float HitungLuas()
+{
+    return Panjang * Lebar;
+}
+void TampilData()
+{
+    cout << "Luas Persegi Panjang :" << HitungLuas();
+}
+
+int main() {
+    InputData();
+    TampilData();
+    return 0;
+}
